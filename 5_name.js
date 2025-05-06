7dd22cb114eb5ff6c7273b4afd7a2ec48facb9b1
Farraz Fauzan <farrazfauzan42@gmail.com>
2025-05-06 18:41:07 +0700

    Update 5_name.js

--- 5_name.js
+++ 5_name.js
@@ -1,6 +1,6 @@
 let fullName = "Farraz Fauzan";
-let names = fullName.split(" "); // pisah berdasarkan spasi
+let names = fullName.split(" "); 
 let initials = names[0][0].toUpperCase() + names[1][0].toUpperCase();
 
 console.log("Inisial: " + initials);
-// Output: JD
+
